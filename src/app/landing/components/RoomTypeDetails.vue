32d2b2acec91638126f98bab80a5731d7b34a63a
KareemWebDeveloper <116171727+KareemWebDeveloper@users.noreply.github.com>
2024-10-01 01:19:47 +0300

    room type details pop up

--- src/app/landing/components/RoomTypeDetails.vue
+++ src/app/landing/components/RoomTypeDetails.vue
@@ -1,14 +1,48 @@
 <script setup lang="ts">
-import { inject, ref } from "vue";
+import { inject, onMounted, ref } from "vue";
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { EffectCoverflow, Pagination, Autoplay, Keyboard } from 'swiper/core';
+import AppImage from '@/common/components/AppImage.vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow'
+const modules = [Autoplay, Keyboard, EffectCoverflow, Pagination]
 import { PropertyFindFilteredReservableUnit } from '@buf/ahmeddarwish_abc-api.bufbuild_es/abc/v1/properties_property_filters_pb';
-
+import apiClient from "@/common/api/ApiClient";
+import { ReservableUnitsViewEntity } from "@buf/ahmeddarwish_abc-api.bufbuild_es/abc/v1/properties_entities_pb";
+import Loading from "@/common/components/Loading.vue";
 const dialogRef: any = inject('dialogRef');
 const room = ref<PropertyFindFilteredReservableUnit>(dialogRef.value.data.room)
+const reservableUnit = ref<ReservableUnitsViewEntity>()
+
+onMounted(() => {
+    apiClient.reservableUnitFind({recordId : room.value.reservableUnitId}).then((result) => {
+        setTimeout(() => {
+            reservableUnit.value = result
+        }, 300);
+    }).catch((err) => {
+        
+    });
+})
+
 </script>
 
-
 <template>
-    <div>
-        {{ room }}
+    <div v-if="reservableUnit" class="zoomin animation-duration-300">
+        <swiper class="slide-innn" :modules="modules" :pagination="true" :effect="'coverflow'" :grab-cursor="true"
+        :keyboard="{ enabled: true }" :centered-slides="false" :slides-per-view="2" :autoplay="{
+          delay : 3000,
+          pauseOnMouseEnter : true,
+        }" :coverflow-effect="{ rotate: 50, stretch: 0, depth: 100, modifier: 1,slideShadows: false}">
+        <swiper-slide v-for="image in reservableUnit?.reservableUnitImages.split(',')">
+          <AppImage :src="image" :image-props="{ preview: true }" :size="{ width: '100%' }" max-height='200px'></AppImage>
+        </swiper-slide>
+      </swiper>
+      <p class="text-center text-base p-2 px-3">
+          {{ reservableUnit.reservableUnitDescription }}
+      </p>
+    </div>
+    <div v-else class="zoomin animation-duration-100 justify-content-center flex align-items-center w-full h-30rem">
+        <Loading></Loading>
     </div>
 </template>
--- src/app/landing/components/partials/RoomType.vue
+++ src/app/landing/components/partials/RoomType.vue
@@ -1,48 +1,49 @@
 <script setup lang="ts">
 import { PropertyFindFilteredReservableUnit } from '@buf/ahmeddarwish_abc-api.bufbuild_es/abc/v1/properties_property_filters_pb';
 import Button from 'primevue/button';
-//import { useDialog } from 'primevue/usedialog';
-//import RoomTypeDettails from '../RoomTypeDetails.vue';
+import { useDialog } from 'primevue/usedialog';
+import RoomTypeDetails from '../RoomTypeDetails.vue';
 import { getAssetPath } from '@/common/utilites/path';
-//const dialog = useDialog();
+const dialog = useDialog();
 
 const props = defineProps<{
 	unit: PropertyFindFilteredReservableUnit
 }>()
 
-//const openRoomTypeDialog = () => {
-//	dialog.open(RoomTypeDettails, {
-//		props: {
-//			style: {
-//				width: '50vw',
-//			},
-//			breakpoints: {
-//				'960px': '75vw',
-//				'640px': '90vw'
-//			},
-//			modal: true,
-//			dismissableMask: true
-//		},
-//		data: {
-//			room: props.unit
-//		},
-//		onClose: (options) => {
-//			console.log('options', options)
-//		}
-//
-//	});
-//}
+const openReservableUnitFindDialog = () => {
+	dialog.open(RoomTypeDetails, {
+		props: {
+			style : {'width' : '50vw'},
+			header : props.unit.reservableUnitName,
+			closable : true,
+			modal: true,
+			dismissableMask: true
+		},
+		data: {
+			room: props.unit
+		},
+		onClose: (options) => {
+			console.log('options', options)
+		}
+
+	});
+}
 const imagePath = getAssetPath(props.unit.reservableUnitImage)
 </script>
 
 <template>
 	<div class="room-card " :style="{ backgroundImage: `url(${imagePath})` }">
 		<div class="w-full h-full flex justify-content-center align-items-end pb-5">
-			<Button @click="" :label="unit.reservableUnitName" severity="contrast"></Button>
+			<Button @click="openReservableUnitFindDialog" :label="unit.reservableUnitName" severity="contrast"></Button>
 		</div>
 	</div>
 </template>
 <style>
+@media screen and (max-width : 600px) {
+	.p-dialog{
+		width: 95vw !important;
+	}
+}
 .room-overlay {
 	background-color: rgba(0, 0, 0, 0.119) !important;
 	padding: 1rem;
