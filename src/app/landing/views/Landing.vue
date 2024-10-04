4757bafc6e4c039510301ff961f26fa37822f762
darwishdev <a.darwish.dev@gmail.com>
2024-10-04 05:22:51 +0300

    finish hiba edits

--- src/app/landing/views/Landing.vue
+++ src/app/landing/views/Landing.vue
@@ -12,7 +12,7 @@ import VideoBanner from '../components/VideoBanner.vue';
 import Discover from '../components/Discover.vue';
 import FeaturedAmenities from '../components/FeaturedAmenities.vue';
 const globalStore = useGlobalStore()
-const { bannerImage, featuredAmenitiesImage } = globalStore.websiteFindResponse.website
+const { bannerImage, featuredAmenitiesImage, bannerHeadline, bannerText } = globalStore.websiteFindResponse.website
 
 </script>
 <template>
@@ -21,28 +21,25 @@ const { bannerImage, featuredAmenitiesImage } = globalStore.websiteFindResponse.
 
 		<Slider></Slider>
 		<div class="separator my-5"></div>
-		<h3 class="text-center animate-on-scroll fade-up text-base md:text-lg px-1 md:px-5 md:py-3 ">{{
-			globalStore.websiteFindResponse.property.propertyDescription }}</h3>
+		<p class="text-center animate-on-scroll fade-up text-base md:text-lg px-1 md:px-5 md:py-3 "
+			v-html="globalStore.websiteFindResponse.property.propertyDescription">
+		</p>
 	</AppSection>
 	<Banner class :bannerImage="bannerImage!">
-		<div class="container">
-			<div class="text-white px-2 w-full">
-				<h1 class="text-white">
-					Live the Stay, Love the Location
-				</h1>
-				<h3>
-					Enjoy your stay at Rhactus House San Stefano
-				</h3>
-			</div>
+		<div class="text-white px-2 w-full banner-text">
+
+			<h1 class="text-white">{{ bannerHeadline }}
+			</h1>
+			<h3>
+				{{ bannerText }}
+			</h3>
 		</div>
 	</Banner>
-	<div id="amenities" class="py-8 darker">
-		<div class="wrapper">
-			<h2 class="reveal">Amenities</h2>
-			<hr class="short">
-		</div>
+
+	<AppSection dark id="amenities" title="Amenities">
 		<Amenities></Amenities>
-	</div>
+	</AppSection>
+
 	<AppSection id="rooms" title="Apartment Types">
 		<RoomTypes></RoomTypes>
 	</AppSection>
@@ -66,15 +63,7 @@ const { bannerImage, featuredAmenitiesImage } = globalStore.websiteFindResponse.
 	background-color: var(--p-color-background);
 }
 
-.amenities-banner.banner {
-	padding: var(--p-larg-gap);
-	height: 500px !important;
-	background-size: cover;
-
-	@media (max-width:400px) {
-		height: 600px !important;
-	}
-}
+.amenities-banner.banner {}
 
 .amenities {
 	display: grid;
